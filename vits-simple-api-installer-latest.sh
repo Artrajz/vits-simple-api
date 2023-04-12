59b5a8c259901af96bc583ea2fb9b703dc9f4f84
Emberstar <969242373@qq.com>
2023-04-12 17:22:11 +0800

    update:docker script

--- vits-simple-api-installer-latest.sh
+++ vits-simple-api-installer-latest.sh
@@ -1,4 +1,4 @@
-INSTALL_DIR=/usr/local/moegoe-simple-api
+INSTALL_DIR=/usr/local/vits-simple-api
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -8,21 +8,21 @@ PLAIN='\033[0m'
 mkdir -p $INSTALL_DIR
 cd $INSTALL_DIR
 if [ ! -f config.py ]; then
-    echo -e "${YELLOW}下载config.py\n${PLAIN}"
-    wget -O $INSTALL_DIR/config.py https://raw.githubusercontent.com/Artrajz/MoeGoe-Simple-API/main/config.py
+    echo -e "${YELLOW}download config.py\n${PLAIN}"
+    wget -O $INSTALL_DIR/config.py https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/config.py
 fi
 
-wget -O $INSTALL_DIR/docker-compose.yaml https://raw.githubusercontent.com/Artrajz/MoeGoe-Simple-API/main/docker-compose.yaml
+wget -O $INSTALL_DIR/docker-compose.yaml https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/docker-compose.yaml
 
 echo -e "${YELLOW}即将开始拉取镜像${PLAIN}"
-echo -e "${YELLOW}镜像大小为5g，拉取会比较慢\n${PLAIN}"
+echo -e "${YELLOW}镜像大小为5GB，拉取会比较慢\n${PLAIN}"
 
 docker compose pull
 docker compose up -d
 
-echo -e "\n已成功升级/安装MoeGoe-simple-api"
+echo -e "\n已成功升级/安装vits-simple-api"
 echo -e "配置文件目录为 $(realpath $INSTALL_DIR)"
-echo -e "${RED}2023.4.7更新-需要更新config.py，否则会报错${PLAIN}"
+echo -e "${RED}2023.4.12更新-需要更新config.py，否则会报错${PLAIN}"
 echo -e "${YELLOW}如未导入vits模型则无法使用，请在配置文件目录中导入模型${PLAIN}"
 echo -e "修改配置文件后后需重新启动docker容器才能生效"
 echo -e "${YELLOW}如有疑问可以在issues中提出${PLAIN}"
