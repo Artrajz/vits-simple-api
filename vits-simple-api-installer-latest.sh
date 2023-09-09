7a9f4b78224a4bc4dd12f48fe54615513370a1b0
Emberstar <969242373@qq.com>
2023-09-09 16:45:37 +0800

    update: gpu docker

--- vits-simple-api-installer-latest.sh
+++ vits-simple-api-installer-latest.sh
@@ -7,14 +7,41 @@ PLAIN='\033[0m'
 
 mkdir -p $INSTALL_DIR
 cd $INSTALL_DIR
+
+download_with_fallback() {
+    local filename=$1
+    shift  # Shift arguments to the left to handle URLs
+
+    local success=0
+    local url
+    for url in "$@"; do
+        echo -e "${YELLOW}Attempting to download $filename from $url\n${PLAIN}"
+        if wget -O "$INSTALL_DIR/$filename" "$url"; then
+            success=1
+            break
+        fi
+    done
+
+    if [ "$success" -ne 1 ]; then
+        echo -e "${RED}Failed to download $filename from all provided URLs.${PLAIN}"
+        exit 1
+    fi
+}
+
+version_gt() {
+    test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"
+}
+
 if [ ! -f config.py ]; then
-    echo -e "${YELLOW}download config.py\n${PLAIN}"
-    wget -O $INSTALL_DIR/config.py https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/config.py
+    download_with_fallback config.py \
+        "https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/config.py" \
+        "https://ghproxy.com/https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/config.py"
 fi
 
 if [ ! -f gunicorn_config.py ]; then
-    echo -e "${YELLOW}download config.py\n${PLAIN}"
-    wget -O $INSTALL_DIR/gunicorn_config.py https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/gunicorn_config.py
+    download_with_fallback gunicorn_config.py \
+        "https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/gunicorn_config.py" \
+        "https://ghproxy.com/https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/gunicorn_config.py"
 fi
 
 while true; do
@@ -24,13 +51,15 @@ while true; do
     read -p "Enter your choice (1 or 2): " choice
     case $choice in
         1)
-            echo -e "${YELLOW}Downloading docker-compose.yaml (CPU version)\n${PLAIN}"
-            wget -O $INSTALL_DIR/docker-compose.yaml https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/docker-compose.yaml
+            download_with_fallback docker-compose.yaml \
+                "https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/docker-compose.yaml" \
+                "https://ghproxy.com/https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/docker-compose.yaml"
             break
             ;;
         2)
-            echo -e "${YELLOW}Downloading docker-compose-gpu.yaml (GPU version)\n${PLAIN}"
-            wget -O $INSTALL_DIR/docker-compose.yaml https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/docker-compose-gpu.yaml
+            download_with_fallback docker-compose.yaml \
+                "https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/docker-compose-gpu.yaml" \
+                "https://ghproxy.com/https://raw.githubusercontent.com/Artrajz/vits-simple-api/main/docker-compose-gpu.yaml"
             break
             ;;
         *)
@@ -39,10 +68,32 @@ while true; do
     esac
 done
 
+if [ "$choice" -eq 2 ]; then
+    DOCKER_VERSION=$(docker version --format '{{.Server.Version}}')
+    MIN_DOCKER_VERSION="19.03"
+
+    if version_gt $MIN_DOCKER_VERSION $DOCKER_VERSION; then
+        echo -e "${RED}Your Docker version ($DOCKER_VERSION) does not support GPU. You need at least version $MIN_DOCKER_VERSION.${PLAIN}"
+        exit 1
+    fi
+fi
+
+if ! command -v docker-compose &> /dev/null; then
+    echo -e "${RED}docker-compose could not be found.${PLAIN}"
+    exit 1
+fi
+
 echo -e "${YELLOW}Pulling the image might take a while, so why not grab a cup of java first?\n${PLAIN}"
 
 docker compose pull
 docker compose up -d
+
+if [ "$choice" -eq 2 ]; then
+    if ! docker run --gpus all artrajz/vits-simple-api:latest-gpu nvidia-smi &> /dev/null; then
+        echo -e "${RED}Your Docker does not seem to support GPU or NVIDIA Docker is not installed properly.${PLAIN}"
+        exit 1
+    fi
+fi
 
 echo -e "\nThe upgrade or installation has been completed."
 echo -e "The configuration file directory is $(realpath $INSTALL_DIR)"
