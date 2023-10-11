332ce175117d5853bd17fab73a49210e32ea1a5e
Artrajz <969242373@qq.com>
2023-10-11 16:57:35 +0800

    update: filter language tags

--- utils/classify_language.py
+++ utils/classify_language.py
@@ -2,16 +2,27 @@ from config import LANGUAGE_IDENTIFICATION_LIBRARY
 
 module = LANGUAGE_IDENTIFICATION_LIBRARY.lower()
 
+langid_languages = ["af", "am", "an", "ar", "as", "az", "be", "bg", "bn", "br", "bs", "ca", "cs", "cy", "da", "de", "dz", "el",
+             "en", "eo", "es", "et", "eu", "fa", "fi", "fo", "fr", "ga", "gl", "gu", "he", "hi", "hr", "ht", "hu", "hy",
+             "id", "is", "it", "ja", "jv", "ka", "kk", "km", "kn", "ko", "ku", "ky", "la", "lb", "lo", "lt", "lv", "mg",
+             "mk", "ml", "mn", "mr", "ms", "mt", "nb", "ne", "nl", "nn", "no", "oc", "or", "pa", "pl", "ps", "pt", "qu",
+             "ro", "ru", "rw", "se", "si", "sk", "sl", "sq", "sr", "sv", "sw", "ta", "te", "th", "tl", "tr", "ug", "uk",
+             "ur", "vi", "vo", "wa", "xh", "zh", "zu"]
+
 
 def classify_language(text: str, target_languages: list = None) -> str:
     if module == "fastlid" or module == "fasttext":
-        from fastlid import fastlid
+        from fastlid import fastlid, supported_langs
         classifier = fastlid
-        if target_languages != None: fastlid.set_languages = target_languages
+        if target_languages != None:
+            target_languages = [lang for lang in target_languages if lang in supported_langs]
+            fastlid.set_languages = target_languages
     elif module == "langid":
         import langid
         classifier = langid.classify
-        if target_languages != None: langid.set_languages(target_languages)
+        if target_languages != None:
+            target_languages = [lang for lang in target_languages if lang in langid_languages]
+            langid.set_languages(target_languages)
     else:
         raise ValueError(f"Wrong LANGUAGE_IDENTIFICATION_LIBRARY in config.py")
 
