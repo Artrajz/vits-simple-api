76d458d8d5877fe07403dee5ffebd1521a25fdb8
Artrajz <969242373@qq.com>
2023-10-31 00:14:09 +0800

    Update global_config

--- utils/config_manager.py
+++ utils/config_manager.py
@@ -1,15 +1,42 @@
 import os
 import shutil
+import logging
 
 import torch
 import yaml
 
 import config as default_config
-from logger import logger
 
 YAML_CONFIG_FILE = os.path.join(default_config.ABS_PATH, 'config.yml')
 
-global_config = {}
+
+class Config:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def items(self):
+        return self.__dict__.items()
+
+    def keys(self):
+        return self.__dict__.keys()
+
+    def values(self):
+        return self.__dict__.values()
+
+    def update(self, data: dict):
+        self.__dict__.update(data)
+
+
+global_config = Config()
 
 
 # 修改表示和构造器的定义
@@ -29,7 +56,9 @@ yaml.add_constructor('!torch.device', construct_torch_device, Loader=yaml.SafeLo
 def load_yaml_config(filename):
     with open(filename, 'r') as f:
         yaml_config = yaml.safe_load(f)
-    logger.info(f"Loading yaml from {YAML_CONFIG_FILE}")
+    global global_config
+    global_config.update(yaml_config)
+    logging.info(f"Loading yaml from {YAML_CONFIG_FILE}")
     return yaml_config
 
 
@@ -38,14 +67,14 @@ def save_yaml_config(filename, data):
     try:
         with open(temp_filename, 'w') as f:
             yaml.safe_dump(data, f)
-
+        global global_config
+        global_config.update(data)
         shutil.move(temp_filename, filename)
-        logger.info(f"Saving yaml to {YAML_CONFIG_FILE}")
+        logging.info(f"Saving yaml to {YAML_CONFIG_FILE}")
     except Exception as e:
-        logger.error(f"Error while saving yaml: {e}")
+        logging.error(f"Error while saving yaml: {e}")
         if os.path.exists(temp_filename):
             os.remove(temp_filename)
-
 
 
 model_path = ["MODEL_LIST", "HUBERT_SOFT_MODEL", "DIMENSIONAL_EMOTION_NPY", "DIMENSIONAL_EMOTION_MODEL"]
@@ -56,7 +85,7 @@ if os.path.exists(YAML_CONFIG_FILE):
 else:
     global_config.setdefault("model_path", {})
     global_config.setdefault("default_parameter", {})
-    
+
     for key, value in vars(default_config).items():
         if key.islower():
             continue
