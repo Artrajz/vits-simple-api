d8c2000a6420841eecb8dc63ba2f9e81dbaabf4e
Artrajz <969242373@qq.com>
2023-11-09 16:24:49 +0800

    Fix version tips.

--- bert_vits2/utils.py
+++ bert_vits2/utils.py
@@ -42,17 +42,17 @@ def load_checkpoint(checkpoint_path, model, optimizer=None, skip_optimizer=False
                 if version is None:
                     logger.error(f"{k} is not in the checkpoint")
                     logger.warning(
-                        f"If you're using an older version of the model, consider adding the \"version\" parameter to the model's config.json under the \"data\" section. For instance: \"legacy_version\": \"1.0.1\"")
+                        f"If you're using an older version of the model, consider adding the \"version\" parameter to the model's config.json. For instance: \"version\": \"1.0.1\"")
             elif "flow.flows.0.enc.attn_layers.3" in k:
                 logger.error(f"{k} is not in the checkpoint")
                 logger.warning(
-                    f"If you're using a transitional version, please add the \"version\": \"1.1.0-transition\" parameter within the \"data\" section of the model's config.json.")
+                    f"If you're using a transitional version, please add the \"version\": \"1.1.0-transition\" parameter to the model's config.json. For instance: \"version\": \"1.1.0-transition\"")
             elif "en_bert_proj" in k:
                 v = torch.zeros_like(v)
                 if version is None:
                     logger.error(f"{k} is not in the checkpoint")
                     logger.warning(
-                        f"If you're using an older version of the model, consider adding the \"version\" parameter to the model's config.json under the \"data\" section. For instance: \"legacy_version\": \"1.1.1\"")
+                        f"If you're using an older version of the model, consider adding the \"version\" parameter to the model's config.json. For instance: \"version\": \"1.1.1\"")
             else:
                 logger.error(f"{k} is not in the checkpoint")
 
